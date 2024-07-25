66054020db778ac2eb0fc182e300107c1084d20d
Iranna1233 <77201539+Iranna1233@users.noreply.github.com>
2024-07-25 19:02:35 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "us-east-1"  # Replace this with your desired region
 }
 
-resource "aws_s3_bucket" "my-tf-test-bucket1" {
-  bucket = "my-tf-test-bucket1"
+resource "aws_s3_bucket" "mw-test-bucket1" {
+  bucket = "mw-test-bucket1"
   acl    = "private"
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "my-tf-test-bucket1" {
-  bucket = aws_s3_bucket.my-tf-test-bucket1.id
+resource "aws_s3_bucket_lifecycle_configuration" "mw-test-bucket1" {
+  bucket = aws_s3_bucket.mw-test-bucket1.id
   rule {
     id = "archive"
     status = "Enabled"
