0eb636d6204abc1ee94aba088b523c18794c1e16
Iranna1233 <77201539+Iranna1233@users.noreply.github.com>
2024-12-04 22:55:34 +0530

    Update s3.tf scripts

--- s3.tf
+++ s3.tf
@@ -2,19 +2,34 @@ provider "aws" {
   region = "us-east-1"  # Replace this with your desired region
 }
 
-resource "aws_s3_bucket" "mw-test-bucket1" {
-  bucket = "mw-test-bucket1"
+resource "aws_s3_bucket" "middleware-s3-dev-bucket" {
+  bucket = "middleware-s3-dev-bucket"
   acl    = "private"
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "mw-test-bucket1" {
-  bucket = aws_s3_bucket.mw-test-bucket1.id
+resource "aws_s3_bucket_versioning" "versioning-middleware-s3-dev-bucket" {
+  bucket = aws_s3_bucket.middleware-s3-dev-bucket.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "middleware-s3-dev-bucket" {
+  bucket = aws_s3_bucket.middleware-s3-dev-bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "middleware-s3-dev-bucket" {
+  bucket = aws_s3_bucket.middleware-s3-dev-bucket.id
   rule {
-    id = "archive"
-    status = "Enabled"
-    transition {
-      days          = 1
-      storage_class = "GLACIER"
+    apply_server_side_encryption_by_default {
+      sse_algorithm   = "AES256"
     }
   }
 }
+
+
