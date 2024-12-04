83c7bfaeb367e52fd6fcaafb941ee201928c7bde
Iranna1233 <77201539+Iranna1233@users.noreply.github.com>
2024-12-05 00:06:22 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "Demo" {
+  ami       = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  key_name = "graviton"
+  tags = {
+    Name = "Demo"
+  }
+}
